cae28095372398d53befc195c00a219a8fbed658
luanvu123 <phamvuluan131@gmail.com>
2023-05-26 12:24:58 +0700

    update emailsent email

--- resources/views/pages/thankyou.blade.php
+++ resources/views/pages/thankyou.blade.php
@@ -7,7 +7,7 @@
     <h2>Cảm ơn bạn đã thanh toán thành công!</h2>
     <p>Dưới đây là chi tiết đơn hàng của bạn:</p>
     <p>Mã đơn hàng: {{ $orderId }}</p>
-    <p>Số tiền: {{ $amount }}</p>
+    <p>Số tiền: {{ number_format($amount, 0, ',', '.') }} VNĐ</p>
 
 </body>
 </html>
